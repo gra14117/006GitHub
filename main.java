8653c44b64b8af3419373e3a129b0191310befdf
gra14117 <gra14117@spengergasse.at>
2013-05-16 15:55:07 +0200

    Personal hello

--- main.java
+++ main.java
@@ -1,10 +1,15 @@
-
+import java.util.Scanner;
 public class main 
 {
 
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
+		Scanner user_input = new Scanner( System.in );
+		String firstname;
+		System.out.print("Enter your  firstname: ");
+		firstname = user_input.next( );
+		System.out.println("Hello, "+firstname+"!");
 	}
 
 }
